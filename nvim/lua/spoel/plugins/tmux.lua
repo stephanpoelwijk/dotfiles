acee67a13ac4dcc1f50e3e96dc7239cad17e13e5
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2024-09-03 13:54:56 +0200

    Use same keymap setup for tmux.lua

--- nvim/lua/spoel/plugins/tmux.lua
+++ nvim/lua/spoel/plugins/tmux.lua
@@ -6,12 +6,13 @@ return {
 		nvim_tmux_nav.setup({
 			disable_when_zoomed = true, -- defaults to false
 		})
+		local keymap = vim.keymap -- for conciseness
 
-		vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-		vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-		vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-		vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-		vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-		vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+		keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+		keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+		keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+		keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+		keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+		keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 	end,
 }
