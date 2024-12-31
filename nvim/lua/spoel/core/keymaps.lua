063c5ed2c21f14d24342c7f54e9ba23aa955ae2b
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2024-12-31 22:17:01 +0100

    Remove jk keymap because it does not work with Dutch :)

--- nvim/lua/spoel/core/keymaps.lua
+++ nvim/lua/spoel/core/keymaps.lua
@@ -6,9 +6,6 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps -------------------
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
