ad2adf25d23275c3d88f22e9dfab6e7e07546834
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-20 18:15:21 +0100

    Configure mason which servers to install

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,5 +8,44 @@ return {
   },
   config = function() 
 
+    local mason = require('mason')
+    local masonLspConfig = require('mason-lspconfig')
+    local masonToolInstaller = require('mason-tool-installer')
+
+    mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+    })
+
+    masonLspConfig.setup({
+			ensure_installed = {
+        "csharp_ls",
+        "cssls",
+        "eslint",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "sqlls",
+        "tailwindcss",
+        "ts_ls",
+        "yamlls",
+			},
+    })
+
+    masonToolInstaller.setup({
+			--  		ensure_installed = {
+			-- 	"prettier", -- prettier formatter
+			-- 	"stylua", -- lua formatter
+			-- 	-- "black", -- python formatter,
+			-- 	-- "pylint",
+			-- 	"eslint_d",
+			-- },
+    })
+
   end,
 }
