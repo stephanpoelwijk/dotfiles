972ccc8e540d1fe2e12283306dc62a16f594994b
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-20 17:28:58 +0100

    Add empty lsp config

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function() 
+
+  end,
+}
