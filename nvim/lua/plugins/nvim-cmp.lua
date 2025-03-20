91ae2c557bf0a1d506c0acd6ec03b06469b9d10c
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-20 19:21:05 +0100

    Make completion work better

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -40,7 +40,7 @@ return {
     local luaSnipCompletionOptions = {
       expandable_indicator = true,
       expand = function(args)
-        luaSnip.lsp_expand(args)
+        luaSnip.lsp_expand(args.body)
       end
     }
 
@@ -55,6 +55,15 @@ return {
     cmp.setup({
       completion = { completeopt = "menu,menuone,noinsert" },
       snippet = luaSnipCompletionOptions,
+       mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
       window = windowOptions,
       sources = cmp.config.sources({
 					{ name = "nvim_lsp", group_index = 1 }, -- lsp
