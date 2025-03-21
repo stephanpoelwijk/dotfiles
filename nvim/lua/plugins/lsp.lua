4630a34551d1f1e631f67347e110b9753d2b9bc3
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-21 09:50:19 +0100

    Add hint to lsp settings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -58,6 +58,7 @@ return {
 				function(server_name)
 					lspconfig[server_name].setup({
 						capabilities = capabilities,
+						-- settings = {},
 					})
 				end,
 				["lua_ls"] = function()
