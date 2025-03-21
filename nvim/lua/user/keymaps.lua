9baaa56b257c22b8dc926f4af6f4392b8b1c25e2
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-21 09:14:18 +0100

    Add keymap for lazygit

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,23 +1,21 @@
 local M = {}
 
-
 local keymap = vim.keymap
 
 -- Misc stuff
 keymap.set("n", "<Esc", ":nohlsearch<CR>", { desc = "Clear search highlights" })
 
-
 -- Telescope
-local builtin = require('telescope.builtin')
-keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
+local builtin = require("telescope.builtin")
+keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 -- Oil
-keymap.set('n', '-', "<CMD>Oil<CR>", { desc = "Open parent directory" })
+keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-
+-- Utilities
+keymap.set("n", "<leader>lg", "<CMD>LazyGit<CR>", { desc = "Open LazyGit" })
 
 return M
