87de2b83ad4bbbe058a7598ac562a5ff95918152
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-21 12:55:11 +0100

    Set tabs to 4 spaces

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -6,13 +6,13 @@ vim.opt.rnu = true
 vim.opt.showmode = false
 
 -- Set tabs to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 -- Enable auto indenting and set it to spaces
 vim.opt.smartindent = true
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 
 -- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
 vim.opt.breakindent = true
