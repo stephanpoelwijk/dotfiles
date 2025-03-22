174acd65927f3c39ff05112497b5ba3c06ab2d76
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-22 22:07:47 +0100

    Split out dump() into a utils folder

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,21 +15,6 @@ return {
 			local masonLspConfig = require("mason-lspconfig")
 			local masonToolInstaller = require("mason-tool-installer")
 
-			local function dump(o)
-				if type(o) == "table" then
-					local s = "{ "
-					for k, v in pairs(o) do
-						if type(k) ~= "number" then
-							k = '"' .. k .. '"'
-						end
-						s = s .. "[" .. k .. "] = " .. dump(v) .. ","
-					end
-					return s .. "} "
-				else
-					return tostring(o)
-				end
-			end
-
 			mason.setup({
 				ui = {
 					icons = {
@@ -71,8 +56,6 @@ return {
 			-- local capabilities = cmp_nvim_lsp.default_capabilities()
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-			-- print(dump(capabilities))
 
 			masonLspConfig.setup_handlers({
 				function(server_name)
