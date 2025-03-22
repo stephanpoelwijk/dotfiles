66a7a9000811fd73c478c864d2672bdc98aa4eab
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-22 16:08:16 +0100

    Flesh out React Typescript component

--- nvim/snippets/typescriptreact/component.lua
+++ nvim/snippets/typescriptreact/component.lua
@@ -1,3 +1,23 @@
+-- local t = require("luasnip").text
+
+local bufName = vim.api.nvim_buf_get_name(0)
+
+bufName = vim.fn.expand("%:t:r")
+
 return {
-	require("luasnip").snippet({ trig = "react_component" }, { t("// React component!!") }),
+	require("luasnip").snippet({ trig = "react_component" }, {
+		t({ "export default function " .. bufName .. "({", "" }),
+		t({ "  className,", "" }),
+		t({ "  ...props", "" }),
+		t({ ': React.ComponentProps<"div"> & { }) {', "" }),
+		t({ " return (", "" }),
+		t({ "   <div", "" }),
+		t({ '     className={cn("w-full bg-white", className)}', "" }),
+		t({ "     {...props}", "" }),
+		t({ "   >", "" }),
+		t({ "    </div>", "" }),
+		t({ "  )", "" }),
+		t({ "}", "", "" }),
+		--		t({ "export { PageTitle as PageTitle }", "" }),
+	}),
 }
