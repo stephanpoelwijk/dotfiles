e1df9df630d0164362edec30cd4dce200af75029
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-22 20:56:12 +0100

    Search hidden files and folders with telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,32 @@
 return {
-    'nvim-telescope/telescope.nvim',
-      tag = '0.1.8',
-      dependencies = {
-        'nvim-lua/plenary.nvim'
-      }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
+	},
+	config = function()
+		local telescope = require("telescope")
+		-- local actions = require("telescope.actions")
+
+		telescope.setup({
+			pickers = {
+				live_grep = {
+					file_ignore_patterns = { "node_modules", ".git$", ".venv" },
+					additional_args = function(_)
+						return { "--hidden" }
+					end,
+				},
+				find_files = {
+					hidden = true,
+				},
+			},
+			extensions = { "fzf" },
+		})
+
+		telescope.load_extension("fzf")
+	end,
 }
