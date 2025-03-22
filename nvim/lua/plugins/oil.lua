5633c24b40d91a81ff87eebeae3226072d9138c8
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-22 20:55:54 +0100

    Show hidden files and directories in oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,13 +1,17 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = {
-    { "nvim-tree/nvim-web-devicons", opts = {} }
-  },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	opts = {
+		delete_to_trash = false,
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	-- Optional dependencies
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons", opts = {} },
+	},
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 }
