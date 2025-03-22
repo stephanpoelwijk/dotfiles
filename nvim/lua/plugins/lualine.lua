988a75c35bf297462cb39f93ef67134aba793d0b
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-22 22:37:23 +0100

    Remove unused code

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,27 +1,21 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
 
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        theme = "everforest",
-      },
-      sections = {
-        lualine_x = {
-          -- {
-          --   lazy_status.updates,
-          --   cond = lazy_status.has_updates,
-          --   color = { fg = "#ff9e64" },
-          -- },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				theme = "everforest",
+			},
+			sections = {
+				lualine_x = {
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
