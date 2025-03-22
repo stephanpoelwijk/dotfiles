f091d1d905f297d01a07a2947bf065408124473a
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-03-22 22:46:01 +0100

    Move dump util into its own module

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -1,14 +1,18 @@
-local function dump(o)
+local util = {}
+
+util.dump = function(o)
 	if type(o) == "table" then
 		local s = "{ "
 		for k, v in pairs(o) do
 			if type(k) ~= "number" then
 				k = '"' .. k .. '"'
 			end
-			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+			s = s .. "[" .. k .. "] = " .. util.dump(v) .. ","
 		end
 		return s .. "} "
 	else
 		return tostring(o)
 	end
 end
+
+return util
