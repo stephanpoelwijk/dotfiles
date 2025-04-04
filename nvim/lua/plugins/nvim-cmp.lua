1a2230e711e5cb1b964d8b17e70ece327d265fa1
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-04-04 10:14:22 +0200

    Change autocompletion completion to TAB instead of CR

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -74,7 +74,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<TAB>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			window = windowOptions,
 			sources = cmp.config.sources({
