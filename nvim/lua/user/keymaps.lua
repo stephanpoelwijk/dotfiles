bb7fd64b3161b036854c60f139df866368d8495b
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-04-05 16:44:34 +0200

    Also use F2 for renaming an LSP symbol

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -49,6 +49,7 @@ M.map_lsp_keys = function(buffer)
 
 	opts.desc = "Smart rename"
 	keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+	keymap.set("n", "<F2>", vim.lsp.buf.rename, opts) -- smart rename
 
 	opts.desc = "Show buffer diagnostics"
 	keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
