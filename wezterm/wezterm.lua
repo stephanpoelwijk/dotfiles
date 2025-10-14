75960e566accb6bf39314e9f9904a3e6a509507d
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-10-14 16:02:55 +0200

    Increase font-size by default

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font_size = 15
+config.font_size = 16
 config.color_scheme = "Everforest Dark (Gogh)"
 
 return config
