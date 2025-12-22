73e112085ff17dbdb44f59f5af40769d17a2631b
Stephan Poelwijk <stephan@poelwijkconsultancy.com>
2025-12-22 16:12:28 +0100

    Re-enable custom shortcuts for LSP stuff

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -3,11 +3,11 @@ vim.lsp.set_log_level("debug")
 vim.lsp.enable({ "cssls", "eslint", "html", "jsonls", "yamlls", "lua_ls", "csharp_ls", "ts_ls", "tailwindcss" })
 
 -- Map keys when LSP attaches
--- vim.api.nvim_create_autocmd("LspAttach", {
--- 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
--- 	callback = function(ev)
--- 		require("user.keymaps").map_lsp_keys(ev.buffer)
--- 	end,
--- })
---
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		require("user.keymaps").map_lsp_keys(ev.buffer)
+	end,
+})
+
 require("mason").setup({})
